b5257d36e40a2ad99cb0fd8745e7472db42b2b8b
qwrwed <razk@hotmail.co.uk>
2020-11-15 17:48:10 +0000

    add platform-dependent SafeAreaView

--- src/components/ScreenView.js
+++ src/components/ScreenView.js
@@ -5,38 +5,29 @@ import {
   DefaultTheme,
   Provider as PaperProvider,
 } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 import { styles } from "../styles";
 import { ESLintCompatibleStatusBar as StatusBar } from "./ESLintCompatibleStatusBar";
+import { AppSafeAreaView as SafeAreaView } from "./SafeAreaView";
 
 export const ScreenView = (props) => {
   const colorScheme = useColorScheme();
   const { theme = colorScheme === "dark" ? DarkTheme : DefaultTheme } = {
     props,
   };
-  if (Platform.OS === "web") {
-    return (
-      <PaperProvider theme={theme}>
-        <StatusBar barStyle="auto" />
-        <View style={props.style}>{props.children}</View>;
-      </PaperProvider>
-    );
-  } else {
-    return (
-      <PaperProvider theme={theme}>
-        <StatusBar barStyle="auto" />
-        <SafeAreaView
-          style={[
-            { backgroundColor: theme.colors.background },
-            styles.container,
-            props.style,
-          ]}
-          edges={["right", "left", "top"]}
-        >
-          {props.children}
-        </SafeAreaView>
-      </PaperProvider>
-    );
-  }
+
+  return (
+    <PaperProvider theme={theme}>
+      <StatusBar barStyle="auto" />
+      <SafeAreaView
+        style={[
+          { backgroundColor: theme.colors.background },
+          styles.container,
+          props.style,
+        ]}
+      >
+        {props.children}
+      </SafeAreaView>
+    </PaperProvider>
+  );
 };
